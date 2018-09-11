4e6a0a1d4fa076798553fbf4b049bc0d97d7ff31
Ben Furber <ben@fu.rber.io>
2018-09-11 07:40:01 +0100

    🐛 Export components correctly

--- app/screens/__tests__/feed.test.js
+++ app/screens/__tests__/feed.test.js
@@ -2,7 +2,7 @@ import "react-native";
 import React from "react";
 import {shallow} from "enzyme";
 
-import Feed from "../Feed";
+import {Feed} from "../Feed";
 
 describe("Feed", () => {
   it("renders correctly", () => {
--- app/screens/feed.js
+++ app/screens/feed.js
@@ -31,4 +31,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Feed;
+export {Feed};
