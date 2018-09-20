cea61876c4974f4f042b7787eabe3f56cbd83c1f
Ben Furber <ben@fu.rber.io>
2018-09-20 17:12:52 +0100

    ✨ Add content to Goal screen

--- app/screens/goal.js
+++ app/screens/goal.js
@@ -1,11 +1,47 @@
 import React, { Component } from "react";
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  H1,
+  H3,
+  Header,
+  Text,
+} from "native-base";
 
 class Goal extends Component {
   render() {
+    const { score, tag, title, description } = this.props.details;
+
     return (
-      null
+      <Container>
+        <Header>
+          <H1>
+            {title} ({score}
+            /5)
+          </H1>
+        </Header>
+        <Content>
+          <Text>Part of: {tag}</Text>
+          <Text>{description}</Text>
+
+          <H3>Actions: </H3>
+          <List>{this._renderActions()}</List>
+        </Content>
+      </Container>
     );
-  };
-};
+  }
+
+  _renderActions() {
+    const { actions } = this.props.details;
+
+    return actions.map((action, index) => (
+      <ListItem key={index}>
+        <Text>{action}</Text>
+      </ListItem>
+    ));
+  }
+}
 
 export { Goal };
