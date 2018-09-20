55f438981bc77a8182e57c01e847768d45f8d24c
Ben Furber <ben@fu.rber.io>
2018-09-20 17:13:15 +0100

    ✨ Add Goal screen to storybook

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -3,14 +3,28 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 
 import Welcome from "./Welcome";
-import { GoalSummary } from "../../app/components/GoalSummary";
+import { GoalSummary } from "components";
+import { Goal } from "screens";
 
 storiesOf("Welcome", module).add("to Storybook", () => (
   <Welcome />
 ));
 
+storiesOf("Screens", module)
+  .add("Goal", () => (
+    <Goal
+      details={{
+        actions: ["1", "2"],
+        description: "",
+        score: 4,
+        tag: "Team thoughtbot",
+        title: "I set my own direction",
+      }}
+    />
+  ));
+
 storiesOf("Components", module)
-  .add("goalSummary", () => (
+  .add("GoalSummary", () => (
     <GoalSummary
       details={{
         description: "Hello world 1",
