31180cd239cf5e981fc7ebff688bac8fe9466c06
Ben Furber <ben@fu.rber.io>
2018-10-01 12:01:25 +0100

    ✨ Add empty redux store

--- app/App.js
+++ app/App.js
@@ -2,7 +2,9 @@
 
 import React, { Component } from "react";
 import { createStackNavigator } from "react-navigation";
+import { Provider } from "react-redux";
 
+import store from "appRedux/store";
 import { Feed, Goal } from "screens";
 
 type Props = {};
@@ -19,7 +21,11 @@ const RootStack = createStackNavigator({
 
 class App extends Component<Props> {
   render() {
-    return <RootStack />;
+    return (
+      <Provider store={store}>
+        <RootStack />
+      </Provider>
+    );
   }
 }
 
