62d4dd7144fba5ffdfaeae8c615af461b63cd3b8
Ben Furber <ben@fu.rber.io>
2018-10-03 16:40:35 +0100

    ♻️ Refactor Feed to import goalsData

--- app/screens/Feed.js
+++ app/screens/Feed.js
@@ -8,6 +8,8 @@ import { GoalsList } from "components";
 import { goalsAction } from "redux-modules";
 import type { navigationType } from "types";
 
+import { goalsData } from "../data/goals";
+
 type Props = {
   navigation: navigationType,
 };
@@ -18,51 +20,6 @@ class Feed extends Component<Props> {
   };
 
   render() {
-    const goalsData = {
-      [1]: {
-        actions: ["1", "2"],
-        description: "HELLO",
-        score: 1,
-        tag: "Team thoughtbot",
-        title: "I contribute to thoughtbot being a good place to work",
-      },
-      [2]: {
-        actions: ["1", "2"],
-        description: "",
-        score: 2,
-        tag: "Team thoughtbot",
-        title: "I iterate over my existing knowledge effectively",
-      },
-      [3]: {
-        actions: ["1", "2"],
-        description: "",
-        score: 3,
-        tag: "Team thoughtbot",
-        title: "I’m a great pair programmer",
-      },
-      [4]: {
-        actions: ["1", "2"],
-        description: "",
-        score: 1,
-        tag: "Team thoughtbot",
-        title: "I help others become better ",
-      },
-      [5]: {
-        actions: ["1", "2"],
-        description: "",
-        score: 3,
-        tag: "Team thoughtbot",
-        title: "I’m passionate about software/products",
-      },
-      [6]: {
-        actions: ["1", "2"],
-        description: "",
-        score: 4,
-        tag: "Team thoughtbot",
-        title: "I set my own direction",
-      },
-    };
-
     return (
       <Container>
         <Content>
