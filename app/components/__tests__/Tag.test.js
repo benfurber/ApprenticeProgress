15e0ae0f32c7bda4baecf40a0a91217e81439c8a
Ben Furber <ben@fu.rber.io>
2018-10-04 16:04:13 +0100

    ✨ Add Tag component

---
+++ app/components/__tests__/Tag.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { Tag } from "../Tag";
+
+describe("Tag", () => {
+  it("has a valid snapshot", () => {
+    const component = shallow(<Tag text="I'm team thoughtbot" />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
