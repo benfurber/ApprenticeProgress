ec0497cdf277c30a2233dac0707196958ec597b3
Ben Furber <ben@fu.rber.io>
2018-10-04 16:06:01 +0100

    🔧 Fix ProgressBar test by providing a score

--- app/components/__tests__/ProgressBar.test.js
+++ app/components/__tests__/ProgressBar.test.js
@@ -7,7 +7,7 @@ import { ProgressBar } from "../ProgressBar";
 
 describe("ProgressBar", () => {
   it("has a valid snapshot", () => {
-    const screen = shallow(<ProgressBar />);
+    const screen = shallow(<ProgressBar score={2} />);
 
     expect(screen).toMatchSnapshot();
   });
