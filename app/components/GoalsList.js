16b49480ded948ae21843bbaee25e12cec82fe71
Ben Furber <ben@fu.rber.io>
2018-10-04 19:14:43 +0100

    ✨ Add orderIDsByTitle to GoalsList

--- app/components/GoalsList.js
+++ app/components/GoalsList.js
@@ -5,22 +5,27 @@ import { Content } from "native-base";
 
 import { GoalSummary } from "components";
 import type { GoalsDataType, navigationType } from "types";
+import { orderIDsByTitle } from "utils";
 
 type Props = { goals: GoalsDataType, navigation: navigationType };
 
 class GoalsList extends Component<Props> {
   render() {
     const { goals } = this.props;
-    const goalIds = Object.keys(goals);
+    const goalIds = orderIDsByTitle(goals);
 
     return (
       <Content>
         {goalIds.map(goalId => (
-          <GoalSummary key={goalId} details={goals[goalId]} navigation={this.props.navigation} />
+          <GoalSummary
+            key={goalId}
+            details={goals[goalId]}
+            navigation={this.props.navigation}
+          />
         ))}
       </Content>
     );
-  }
-}
+  };
+};
 
 export { GoalsList };
