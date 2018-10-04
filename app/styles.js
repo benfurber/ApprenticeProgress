4544488f894612ba5ff8750dfd72ad8eb3523a20
Ben Furber <ben@fu.rber.io>
2018-10-04 16:05:22 +0100

    🗑 Drop unused tag styles

--- app/styles.js
+++ app/styles.js
@@ -18,10 +18,7 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     ...standards.elementPadding,
   },
-  tag: {
-    backgroundColor: "green",
-  },
-    row: {
+  row: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
