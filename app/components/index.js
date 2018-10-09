960a4daff9eebed3ce621472edf80e1ed41b8077
Ben Furber <ben@fu.rber.io>
2018-10-09 09:50:22 +0100

    ✨ Add Dot component

--- app/components/index.js
+++ app/components/index.js
@@ -1,4 +1,5 @@
 export { GoalSummary } from "./GoalSummary";
+export { Dot } from "./Dot";
 export { GoalsList } from "./GoalsList";
 export { ProgressBar } from "./ProgressBar";
 export { Tag } from "./Tag";
