bdc74a31d410fadf3709f16440a30fa5c0400f20
Ben Furber <ben@fu.rber.io>
2018-10-10 16:43:08 +0100

    🐛 Fix Apollo build/testing bug
    To enable the tests to pass with Apollo, a change was made which broke the build for newer iOS devices. The new fix means that the app can build and the tests pass. Amazing, right?

--- app/utils/client.js
+++ app/utils/client.js
@@ -1,5 +1,4 @@
 import ApolloClient from "apollo-boost";
-import "isomorphic-fetch";
 
 const client = new ApolloClient({
   uri: "https://apprentice-progress-api.herokuapp.com/",
--- jest-setup.js
+++ jest-setup.js
@@ -2,3 +2,7 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// To sort fetch issue with Apollo
+// https://stackoverflow.com/questions/50688998/using-apolloclient-with-node-js-fetch-is-not-found-globally-and-no-fetcher-pas
+import "cross-fetch/polyfill";
