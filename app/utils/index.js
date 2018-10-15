9070407aa552cb7fb5158aa93ad5474e6b916e9b
Ben Furber <ben@fu.rber.io>
2018-10-15 08:33:44 +0100

    ✨ Add findTitles utility function

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,4 @@
 export { client } from "./client";
+export { findTitles } from "./findTitles";
 export { lastWordOfString } from "./lastWordOfString";
 export { orderByTitle } from "./orderByTitle";
