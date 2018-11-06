e0da571b057685081e08e2f661ac712b1f1d2ccc
Ben Furber <ben@fu.rber.io>
2018-11-06 11:46:38 +0000

    ♻️ Refactor API client to link to dotenv

--- app/utils/client.js
+++ app/utils/client.js
@@ -1,7 +1,8 @@
+import Config from "react-native-config";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://apprentice-progress-api.herokuapp.com/",
+  uri: Config.PRODUCTION_API_ENDPOINT,
 });
 
 export { client };
