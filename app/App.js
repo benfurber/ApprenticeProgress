3e13eb7900ae4e3419e2c75522e0e5eccf25dff7
Ben Furber <ben@fu.rber.io>
2018-11-14 17:35:20 +0000

    ✨ Add Apollo Provider to App

--- app/App.js
+++ app/App.js
@@ -3,12 +3,17 @@
 import React, { Component } from "react";
 import { RootStack } from "./routes";
 
+import { ApolloProvider } from "react-apollo";
+import { client } from "utils";
+
 type Props = {};
 
 class App extends Component<Props> {
   render() {
     return (
+      <ApolloProvider client={client}>
         <RootStack />
+      </ApolloProvider>
     );
   }
 }
