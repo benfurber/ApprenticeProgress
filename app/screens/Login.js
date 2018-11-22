85edc6821c880c93f5dc935f9f12e204004cf451
Ben Furber <ben@fu.rber.io>
2018-11-22 15:15:04 +0000

    ♻️ Refactor Login to auto-login if token present

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,16 +1,41 @@
 // @flow
 
 import React, { Component } from "react";
+import { AsyncStorage } from "react-native";
 import { Content } from "native-base";
 
 import { Background, FormLogin } from "components";
 import type { navigationType } from "types";
+import { handlePress } from "utils";
 
 type Props = {
   navigation: navigationType,
+  email: ?string,
+  password: ?string,
 };
 
 class Login extends Component<Props> {
+  static navigationOptions = {
+    header: null,
+    title: "Login",
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const { navigation } = this.props;
+
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      console.log(token);
+      handlePress("Main", navigation)();
+    }
+  };
+
   render() {
     const { navigation } = this.props;
 
--- app/screens/__tests__/Login.test.js
+++ app/screens/__tests__/Login.test.js
@@ -2,12 +2,22 @@
 
 import React from "react";
 import { shallow } from "enzyme";
+import { AsyncStorage } from "react-native";
 
 import { Login } from "../Login";
+import { navigationMock } from "mocks";
 
 describe("Login", () => {
-  it("has a valid snapshot", () => {
-    const screen = shallow(<Login />);
+  it("has a valid snapshot", async () => {
+    await AsyncStorage.setItem("token", null);
+
+    const screen = shallow(
+      <Login
+        email="ben@testing.com"
+        password="L33tPassword"
+        navigation={navigationMock}
+      />
+    );
 
     expect(screen).toMatchSnapshot();
   });
