2cf313c616b0625b8510bf72f220d72530c803e3
Ben Furber <ben@fu.rber.io>
2018-11-22 16:36:29 +0000

    ✨ Add Logout “screen”

---
+++ app/screens/Logout.js
@@ -0,0 +1,34 @@
+// @flow
+
+import React, { Component } from "react";
+import { AsyncStorage } from "react-native";
+
+import { Pending } from "components";
+import type { navigationType } from "types";
+import { handlePress } from "utils";
+
+type Props = {
+  navigation: navigationType,
+};
+
+class Logout extends Component<Props> {
+  componentDidMount() {
+    const { navigation } = this.props;
+    const clearToken = () => AsyncStorage.removeItem("token");
+
+    const redirectToLogin = handlePress("Authentication", navigation)();
+
+    const logUserOut = async () => {
+      await clearToken();
+      redirectToLogin();
+    };
+
+    return logUserOut();
+  }
+
+  render() {
+    return <Pending condition="Loading" />;
+  }
+}
+
+export { Logout };
--- app/screens/index.js
+++ app/screens/index.js
@@ -1,4 +1,5 @@
 export { Feed, FeedWrapper } from "./Feed";
 export { Goal } from "./Goal";
 export { Login } from "./Login";
+export { Logout } from "./Logout";
 export { Progress, ProgressWrapper } from "./Progress";
